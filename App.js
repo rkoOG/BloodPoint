1e065283aa5edb5b05fd71cfcda09a6c896f7cdb
Rodrigo Meireles <150105587+rkoOG@users.noreply.github.com>
2025-05-15 20:54:46 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Colors from './constants/colors';
+import { useFonts } from 'expo-font';
+import { Provider as PaperProvider } from 'react-native-paper';
+
+
+/*SCREENS*/
+import WelcomeScreen from './screens/WelcomeScreen'; //Tela inicial
+import RegisterScreen from './screens/RegisterScreen';
+import UltimosDadosScreen from './screens/UltimosDadosScreen';
+import HomeScreen from './screens/Home'; //Tela principal 
+import LoginScreen from './screens/Login'; //Tela de login
+/**/
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <StatusBar style='auto'/>
+        <NavigationContainer screenOptions={{headerShown: false}}>
+          <Stack.Navigator>
+            <Stack.Screen name="BloodPoint" component={WelcomeScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="UltimosDados" component={UltimosDadosScreen} />
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+    </PaperProvider>
   );
 }
 
